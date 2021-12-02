b8b6670c170d4dfcfd76019ea2809ab376d7204e
Gediminas Bivainis <gediminas@bivainis.com>
2021-12-02 22:58:07 +0100

    Day 1: solution to part 2 of the challenge

--- day-1/main.go
+++ day-1/main.go
@@ -8,6 +8,58 @@ import (
 	"strconv"
 )
 
+// Returns window size from CLI args.
+func getWindowSize(args []string) int {
+	switch len(args) {
+	case 1, 2:
+		return 1
+	default:
+		var result, _ = strconv.Atoi(os.Args[2])
+		return result
+	}
+}
+
+// Returns a sum of a slice of provided window size in a given array
+func getSumOfNumWindow(numArr []int) int {
+	var windowSize = getWindowSize(os.Args)
+
+	if windowSize > len(numArr) {
+		log.Fatal("Window size cannot be larger than provided array.")
+	}
+
+	sum := 0
+
+	for _, i := range numArr[:windowSize] {
+		sum = sum + i
+	}
+
+	return sum
+}
+
+// Given two CLI arguments:
+// - path to a text file consisting of N numbers per line
+// - an integer of sliding window size.
+//
+// Counts the number of times the sum of measurements in this sliding window increases from the previous sum.
+//
+// Example window of a size 3:
+//   199  A
+//   200  A B
+//   208  A B C
+//   210    B C D
+//   200  E   C D
+//   207  E F   D
+//   240  E F G
+//   269    F G H
+//   260      G H
+//   263        H
+//
+//   The measurements in the first window are marked A (199, 200, 208);
+//   their sum is 199 + 200 + 208 = 607. The second window is marked
+//   B (200, 208, 210); its sum is 618. The sum of measurements in the second
+//   window is larger than the sum of the first, so this first comparison increased.
+// Example usage:
+//   go run input-day-1.txt 3
 func main() {
 	// Initialize empty array to store numbers for each line.
 	var numArray []int
@@ -37,18 +89,30 @@ func main() {
 	file.Close()
 
 	// There is no measurement before the first measurement.
-	var previousNum = numArray[0]
+	var previousNum = getSumOfNumWindow(numArray)
 
 	// Set counter to 0.
 	var increasedCount = 0
 
 	// Increment counter and set new previous number.
-	for _, i := range numArray {
-		if i > previousNum {
+	for index := range numArray {
+		var sum = 0
+		var size = getWindowSize(os.Args)
+		var sliceEndOffset = index + size - 1
+
+		// Prevent from slicing if there are not enough items at the end.
+		if sliceEndOffset < len(numArray) {
+			var slice = numArray[index : index+size]
+
+			sum = getSumOfNumWindow(slice)
+		}
+
+		if sum > previousNum {
 			increasedCount++
 		}
-		previousNum = i
 
+		// Set new starting value to be compared against.
+		previousNum = sum
 	}
 
 	// Print result.
